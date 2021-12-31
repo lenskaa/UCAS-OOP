ce8ac01df535571e40c08a1ff0d47b578873bfe1
Leng Chunlin <lengchunlin19@mails.ucas.ac.cn>
2021-12-31 12:34:05 +0800

    Fix Bug

--- Uno/src/card/WildBlackCard.java
+++ Uno/src/card/WildBlackCard.java
@@ -13,7 +13,7 @@ public class WildBlackCard extends Card {
 	public WildBlackCard(String name, boolean faceup, Point position) {
 		super(name, faceup, position);
 		super.type = 0;
-		if (name.length() == '5') {
+		if (name.length() == 5) {
 			this.func = 0;// wild
 		} else {
 			this.func = 1;// wild4
